3440bca21dc74f15ad0a9141d97c7bfeb202b5d8
Dmitry Ryzhykh <ryzhykh.dmitry@gmail.com>
2016-07-17 20:53:57 +0300

    Task2 added.

---
+++ GenericBinarySearch.Tests/Tests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GenericBinarySearch.Tests
+{
+    [TestFixture]
+    public class SearchElementTests
+    {
+        #region TestData
+        private Comparison<int> comparerInt = Comparer<int>.Default.Compare;
+        public class DataClass
+        {
+            private static string[] arrString = new string[] { "bike", "reading", "programming", "rain", "wind" };
+            public static IEnumerable<TestCaseData> BinarySearchIntData
+            {
+                get
+                {
+                    yield return new TestCaseData(new int[] { 2, 5, 7, 9, 12, 43 }, 2).Returns(0);
+                    yield return new TestCaseData(new int[] { 2, 5, 7, 9, 12, 43 }, 43).Returns(5);
+                    yield return new TestCaseData(new int[] { 2, 5, 7, 9, 12, 43 }, 7).Returns(2);
+                    yield return new TestCaseData(new int[] { 2, 5, 7, 9, 12, 43 }, 13).Returns(-1);
+                }
+            }
+          
+            public static IEnumerable<TestCaseData> BinarySearchStringData
+            {
+                get
+                {
+                    yield return new TestCaseData(arrString, "bike").Returns(0);
+                    yield return new TestCaseData(arrString, "wind").Returns(4);
+                    yield return new TestCaseData(arrString, "programming").Returns(2);
+                    yield return new TestCaseData(arrString, "fear").Returns(-1);
+                    yield return new TestCaseData(new string[] { "sunshine" }, "sunshine").Returns(0);
+                    yield return new TestCaseData(new string[] { "nothing" }, "something").Returns(-1);
+                }
+            }           
+        }        
+        #endregion
+
+        #region Tests with integer
+       
+        [Test, TestCaseSource(typeof(DataClass), "BinarySearchIntData")]
+        public int BinarySearchTests(int[] arr, int item)
+        {
+            return SearchElement<int>.BinarySearch(arr, item, comparerInt);
+        }
+
+        [Test]
+        public void BinarySearchIntExceptionTests()
+        {
+            int[] arr = null;
+            int item = 6;
+            Assert.That(() => { SearchElement<int>.BinarySearch(arr, item); }, Throws.TypeOf<ArgumentNullException>());
+        }
+
+
+        #endregion
+
+        #region Tests with string        
+
+        [Test, TestCaseSource(typeof(DataClass), "BinarySearchStringData")]
+        public int BinarySearchStringTests(string[] arr, string item)
+        {
+            return SearchElement<string>.BinarySearch(arr, item);
+        }
+
+        [Test]
+        public void BinarySearchStringNullExceptionTests()
+        {
+            string[] arr = null;
+            string item = "Weather";
+            Assert.That(() => { SearchElement<string>.BinarySearch(arr, item); }, Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void BinarySearchStringExceptionTests()
+        {
+            string[] arr = new string[] { };
+            string item = "Wind";
+            Assert.That(() => { SearchElement<string>.BinarySearch(arr, item); }, Throws.TypeOf<ArgumentException>());
+        }
+        #endregion
+    }
+}
---
+++ GenericBinarySearch/Search.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBinarySearch
+{
+    /// <summary>
+    /// Search element by Binary algorythm
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public static class SearchElement<T>
+    {
+        #region Public Methods
+        /// <summary>
+        /// Search element in sorted array
+        /// </summary>
+        /// <param name="array">Sorted array</param>
+        /// <param name="item">Element for searching</param>
+        /// <param name="comparer">Rule for comparing elements in array</param>
+        /// <returns>Index of element in array or -1, if it doesn't exist</returns>
+        public static int BinarySearch(T[] array, T item, Comparison<T> comparer = null)
+        {
+            CheckInputArray(array);
+            if (array.Length == 1 && array[0].Equals(item))
+                return 0;
+            if (array.Length == 1 && !array[0].Equals(item))
+                return -1;
+            if (comparer == null)
+                comparer = Comparer<T>.Default.Compare;
+            if (IsSorted(array, comparer) == false)
+                throw new ArgumentException();
+
+            return Search(array, item, comparer);
+        }
+
+        /// <summary>
+        /// Search element in sorted array
+        /// </summary>
+        /// <param name="array">Sorted array</param>
+        /// <param name="item">Element for searching</param>
+        /// <param name="comparer">Rule for comparing elements in array</param>
+        /// <returns>Index of element in array or -1, if it doesn't exist</returns>
+        public static int BinarySearch(T[] array, T item, IComparer<T> comparer)
+        {
+            CheckInputArray(array);
+            if (array.Length == 1 && array[0].Equals(item))
+                return 0;
+            if (array.Length == 1 && !array[0].Equals(item))
+                return -1;
+            if (comparer == null)
+                return BinarySearch(array, item);
+            if (IsSorted(array, comparer.Compare) == false)
+                throw new ArgumentException();
+
+            return Search(array, item, comparer.Compare);
+        }
+        #endregion
+
+        #region Private Methods
+        /// <summary>
+        /// Check array on null and length
+        /// </summary>
+        /// <param name="array">Array</param>
+        private static void CheckInputArray(T[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException();
+            if (array.Length == 0)
+                throw new ArgumentException();
+
+        }
+        /// <summary>
+        /// Check weather the array is sorted
+        /// </summary>
+        /// <param name="arr">Array</param>
+        /// <param name="comparer">Rule for comparing elements in array</param>
+        /// <returns>True, if array is sorted</returns>
+        private static bool IsSorted(T[] arr, Comparison<T> comparer)
+        {
+            bool isGreater = comparer(arr[0], arr[1]) <= 0;
+            for (int i = 1; i < arr.Length;)
+            {
+                if (comparer(arr[i], arr[i++]) <= 0 != isGreater)
+                    return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Search element in sorted array
+        /// </summary>
+        /// <param name="arr">Sorted array</param>
+        /// <param name="item">Element for searching</param>
+        /// <param name="comparer">Rule for comparing elements in array</param>
+        /// <returns>Index of element in array or -1, if it doesn't exist</returns>
+        private static int Search(T[] arr, T item, Comparison<T> comparer)
+        {
+            int low = 0, high = arr.Length;
+            while (low < high)
+            {
+                int mid = (low + high) / 2;
+                if (comparer(item, arr[mid]) == 0)
+                    return mid;
+
+                if (comparer(item, arr[mid]) >= 0)
+                    low = mid + 1;
+                else
+                    high = mid;
+            }
+            return -1;
+        }
+        #endregion
+    }
+}
