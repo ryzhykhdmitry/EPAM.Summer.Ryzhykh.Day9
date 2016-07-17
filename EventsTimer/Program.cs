976e3c1c0ab838832d89db8bd099a5e5323881a8
Dmitry Ryzhykh <ryzhykh.dmitry@gmail.com>
2016-07-17 19:10:49 +0300

    Day 9 Task1 added.

---
+++ EventsTimer/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace EventsTimer
+{
+    /// <summary>
+    /// Event data.
+    /// </summary>
+    public sealed class TimerEventArgs : EventArgs
+    {
+        private readonly string title;
+        private readonly string text;
+
+        public TimerEventArgs(string title, string text)
+        {
+            this.title = title;
+            this.text = text;
+        }
+
+        public string Title { get { return title; } }
+        public string Text { get { return text; } }        
+    }
+
+    /// <summary>
+    /// Allows to make timer with event.
+    /// </summary>
+    public class Timer
+    {
+        private TimerEventArgs eventData;
+        private System.Timers.Timer aTimer; 
+        public event EventHandler<TimerEventArgs> Event = delegate {};
+
+        /// <summary>
+        /// Create timer.
+        /// </summary>
+        /// <param name="time">Time before event.</param>
+        /// <param name="data">Data.</param>
+        /// <param name="repeat">Allows to set, will the event repeat or not.</param>
+        public Timer(int time = 2000, TimerEventArgs data = null, bool repeat = true)
+        {
+            aTimer = new System.Timers.Timer(time);
+            aTimer.AutoReset = repeat;
+            aTimer.Elapsed += TimerEvent;
+            eventData = data;
+        }
+
+        /// <summary>
+        /// Start event timer.
+        /// </summary>
+        public void StartTimer()
+        {
+            aTimer.Enabled = true;
+        }
+        
+        /// <summary>
+        /// Stop event timer.
+        /// </summary>
+        public void StopTimer()
+        {
+            aTimer.Enabled = false;
+        }
+
+
+        protected virtual void OnEvent(TimerEventArgs e)
+        {            
+            EventHandler<TimerEventArgs> temp = Event;
+           
+            if (temp != null)
+            {                
+                temp(this, e);
+            }
+        }
+
+        public void TimerEvent(Object source, ElapsedEventArgs e)
+        {
+            OnEvent(eventData);
+        }
+
+        /// <summary>
+        /// Allows to set event data.
+        /// </summary>
+        /// <param name="e"></param>
+        public void SetEventData(TimerEventArgs e)
+        {
+            eventData = e;
+        }
+    }
+}
---
+++ EventsTimerTests/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventsTimer;
+
+namespace EventsTimerTests
+{
+    class Program
+    {
+        #region User1
+        public class User1
+        {
+            public User1(Timer timer)
+            {
+                Register(timer);
+            }
+            public User1()
+            {
+
+            }
+            private void WakeUp(Object sender, TimerEventArgs message)
+            {
+                Console.WriteLine($"Good morning. I'm John");
+                Console.WriteLine($"Timer says: Title - {message?.Title}, Text - {message?.Text}");
+
+                Console.WriteLine(new string('-', 10));
+            }
+            public void Unregister(Timer timer)
+            {
+                if (ReferenceEquals(timer, null)) throw new ArgumentNullException();
+                timer.Event -= WakeUp;
+            }
+            public void Register(Timer timer)
+            {
+                if (ReferenceEquals(timer, null)) throw new ArgumentNullException();
+                timer.Event += WakeUp;
+            }
+        }
+        #endregion
+
+        #region User2
+        public class User2
+        {
+            public User2(Timer timer)
+            {
+                Register(timer);
+            }
+            public User2()
+            {
+
+            }
+            private void WakeUp(Object sender, TimerEventArgs message)
+            {
+                Console.WriteLine($"Hey! I'm Jake");
+                Console.WriteLine($"Timer says: Title - {message?.Title}, Text - {message?.Text}");
+
+                Console.WriteLine(new string('-', 10));
+            }
+            public void Unregister(Timer timer)
+            {
+                if (ReferenceEquals(timer, null)) throw new ArgumentNullException();
+                timer.Event -= WakeUp;
+            }
+            public void Register(Timer timer)
+            {
+                if (ReferenceEquals(timer, null)) throw new ArgumentNullException();
+                timer.Event += WakeUp;
+            }
+        } 
+        #endregion
+
+        static void Main(string[] args)
+        {
+            Timer timer1 = new Timer(3000);  timer1.SetEventData(new TimerEventArgs("Hello","world"));
+            User1 person1 = new User1();
+            User2 person2 = new User2(timer1);
+            person1.Register(timer1);            
+            timer1.StartTimer();
+            Console.ReadKey();
+            Console.WriteLine("John stoped his registration ");
+            person1.Unregister(timer1);            
+            Console.ReadKey();
+        }
+    }
+}
